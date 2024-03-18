3ceac97abca01d48df2762a7c7a0c2f663034ea9
Pranjal Verma <pvcodes@gmail.com>
2024-03-19 01:21:14 +0530

    better navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import Navbar from "@/components/Navbar";
+// import Link from "next/link";
 // import Image from "next/image";
 // import logo from "@/public/logo.png";
+// import { useRouter } from "next/router";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -11,38 +13,47 @@ export const metadata: Metadata = {
 	description: "A Code Submission Platform",
 };
 
-const Navbar = () => {
-	return (
-		<>
-			<nav className="bg-white border-gray-200 dark:bg-gray-900">
-				<div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-					<Link
-						href="/"
-						className="flex items-center space-x-3 rtl:space-x-reverse"
-					>
-						{/* <Image
-							src={logo}
-							width={30}
-							height={30}
-							alt="Picode Logo"
-						/> */}
-						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-							PiCode
-						</span>
-					</Link>
-					<div className="flex items-center space-x-6 rtl:space-x-reverse">
-						<Link
-							href="/all"
-							className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
-						>
-							All Submission
-						</Link>
-					</div>
-				</div>
-			</nav>
-		</>
-	);
-};
+// const Navbar = () => {
+// 	const router = useRouter();
+
+// 	// Determine the link URL and text based on the current route
+// 	const linkUrl = router.pathname === "/all" ? "/" : "/all";
+// 	const linkText =
+// 		router.pathname === "/all"
+// 			? "Create new Submission"
+// 			: "All Submissions";
+
+// 	return (
+// 		<>
+// 			<nav className="bg-white border-gray-200 dark:bg-gray-900">
+// 				<div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+// 					<Link
+// 						href="/"
+// 						className="flex items-center space-x-3 rtl:space-x-reverse"
+// 					>
+// 						{/* <Image
+//                             src={logo}
+//                             width={30}
+//                             height={30}
+//                             alt="Picode Logo"
+//                         /> */}
+// 						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+// 							PiCode
+// 						</span>
+// 					</Link>
+// 					<div className="flex items-center space-x-6 rtl:space-x-reverse">
+// 						<Link
+// 							href={linkUrl}
+// 							className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
+// 						>
+// 							{linkText}
+// 						</Link>
+// 					</div>
+// 				</div>
+// 			</nav>
+// 		</>
+// 	);
+// };
 
 export default function RootLayout({
 	children,
---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export default function Navbar() {
+	const path = usePathname();
+
+	// Determine the link URL and text based on the current route
+	const linkUrl = path === "/all" ? "/" : "/all";
+	console.log(23, path);
+	const linkText =
+		path === "/all" ? "Create new Submission" : "All Submissions";
+
+	return (
+		<>
+			<nav className="bg-white border-gray-200 dark:bg-gray-900">
+				<div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+					<Link
+						href="/"
+						className="flex items-center space-x-3 rtl:space-x-reverse"
+					>
+						{/* <Image
+                            src={logo}
+                            width={30}
+                            height={30}
+                            alt="Picode Logo"
+                        /> */}
+						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+							PiCode
+						</span>
+					</Link>
+					<div className="flex items-center space-x-6 rtl:space-x-reverse">
+						<Link
+							href={linkUrl}
+							className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
+						>
+							{linkText}
+						</Link>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
+}
